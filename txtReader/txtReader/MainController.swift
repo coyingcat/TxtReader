20526b078ed31e0746837fe43fc6aa25ec648e17
邓江洲 <707703386@qq.com>
2020-08-08 15:07:25 +0800

    简化

--- txtReader/txtReader/MainController.swift
+++ txtReader/txtReader/MainController.swift
@@ -32,16 +32,8 @@ class MainController: ViewController {
         fullButton.backgroundColor = UIColor.green
         fullButton.addTarget(self, action: #selector(fullRead), for: .touchDown)
         view.addSubview(fullButton)
-        fullButton.frame = CGRect(x: (ScreenWidth / 2 - SPACE_SA_100) / 2, y: ScreenHeight - SPACE_SA_150 * 2, width: SPACE_SA_100, height: SPACE_SA_100)
-    //    fullButton.frame = CGRect(x: (ScreenWidth - SPACE_SA_100) / 2, y: ScreenHeight - SPACE_SA_150 * 2, width: SPACE_SA_100, height: SPACE_SA_100)
-        
-        // 快速解析阅读
-        let fastButton = UIButton(type: .custom)
-        fastButton.setTitle("快速解析", for: .normal)
-        fastButton.backgroundColor = UIColor.green
-        fastButton.addTarget(self, action: #selector(fastRead), for: .touchDown)
-        view.addSubview(fastButton)
-        fastButton.frame = CGRect(x: ScreenWidth - SPACE_SA_100 - (ScreenWidth / 2 - SPACE_SA_100) / 2, y: ScreenHeight - SPACE_SA_150 * 2, width: SPACE_SA_100, height: SPACE_SA_100)
+        fullButton.frame = CGRect(x: (ScreenWidth - SPACE_SA_100) / 2, y: ScreenHeight - SPACE_SA_150 * 2, width: SPACE_SA_100, height: SPACE_SA_100)
+  
     }
     
     // 全本解析阅读
@@ -60,24 +52,6 @@ class MainController: ViewController {
             self?.navigationController?.pushViewController(ReadController(reading: readModel), animated: true)
         }
     }
-    
-    // 快速解析阅读
-    @objc private func fastRead() {
-        
-        print("缓存文件地址:", Sand.readDocumentDirectoryPath)
-        
-       
-        
-        guard let url = Bundle.main.url(forResource: "三国演义", withExtension: "txt") else { return }
-        
-        print("快速解析开始时间:",TimerString("YYYY-MM-dd-HH-mm-ss"), Date().timeIntervalSince1970)
-        
-        ReadTextFastParser.parser(least: url) { [weak self] (readModel) in
-            
-            print("快速解析结束时间:",TimerString("YYYY-MM-dd-HH-mm-ss"), Date().timeIntervalSince1970)
-            
-            self?.navigationController?.pushViewController(ReadController(reading: readModel), animated: true)
-        }
-    }
+  
     
 }
