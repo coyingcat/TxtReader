bc6028028b74f2ec3c057e8d83323795b9de4f87
邓江洲 <dengjiangzhouReal@gmail.com>
2021-03-24 11:06:03 +0800

    code lean

--- func/one/three/View/TextContentV.swift
+++ func/one/three/View/TextContentV.swift
@@ -420,24 +420,6 @@ public struct CTStringAttributeName: Hashable, RawRepresentable, CustomStringCon
   }
 
   public static let font = CTStringAttributeName(rawValue: kCTFontAttributeName as String)!
-  public static let kern = CTStringAttributeName(rawValue: kCTKernAttributeName as String)!
-  public static let ligature = CTStringAttributeName(rawValue: kCTLigatureAttributeName as String)!
-  public static let foregroundColor = CTStringAttributeName(rawValue: kCTForegroundColorAttributeName as String)!
-  public static let foregroundColorFromContext = CTStringAttributeName(rawValue: kCTForegroundColorFromContextAttributeName as String)!
-  public static let paragraphStyle = CTStringAttributeName(rawValue: kCTParagraphStyleAttributeName as String)!
-  public static let strokeWidth = CTStringAttributeName(rawValue: kCTStrokeWidthAttributeName as String)!
-  public static let strokeColor = CTStringAttributeName(rawValue: kCTStrokeColorAttributeName as String)!
-  public static let superscript = CTStringAttributeName(rawValue: kCTSuperscriptAttributeName as String)!
-  public static let underlineColor = CTStringAttributeName(rawValue: kCTUnderlineColorAttributeName as String)!
-  public static let underlineStyle = CTStringAttributeName(rawValue: kCTUnderlineStyleAttributeName as String)!
-  public static let verticalForms = CTStringAttributeName(rawValue: kCTVerticalFormsAttributeName as String)!
-  public static let glyphInfo = CTStringAttributeName(rawValue: kCTGlyphInfoAttributeName as String)!
-  public static let runDelegate = CTStringAttributeName(rawValue: kCTRunDelegateAttributeName as String)!
-  public static let baselineOffset = CTStringAttributeName(rawValue: kCTBaselineOffsetAttributeName as String)!
-  public static let trackingAttributeName = CTStringAttributeName(rawValue: kCTTrackingAttributeName as String)!
-  
-  public static let rubyAnnotationAttributeName = CTStringAttributeName(rawValue: kCTRubyAnnotationAttributeName as String)!
-  
   
 }
 
